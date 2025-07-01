f811b2088d2e1f7080b6e2a09c070893e1afb22d
famonterrey13 <famonterrey@uc.cl>
2025-07-01 15:57:36 -0400

    Se agregó función para devolver elección de la computadora

--- rps.js
+++ rps.js
@@ -1,1 +1,17 @@
-console.log("Hola")
+function getComputerChoice(){
+    let NumAuxiliar = parseInt(Math.random()*10);
+    let answer = "er";
+
+    if(NumAuxiliar === 0){
+        answer = "No me pude decidir, tu ganas";
+    } else if (NumAuxiliar > 0 && NumAuxiliar <= 3){
+        answer = "Rock";
+    } else if (NumAuxiliar > 3 && NumAuxiliar <= 6){
+        answer = "Paper";
+    } else if (NumAuxiliar > 7 && NumAuxiliar <= 9){
+        answer = "Scissor";
+    }
+    return answer
+}
+
+console.log(getComputerChoice());
