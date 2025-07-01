d43a23ce69af5f64c61df794872a65613611b2f8
famonterrey13 <famonterrey@uc.cl>
2025-07-01 16:49:02 -0400

    Se agregaron funciones que permiten interacturar con el juego

--- rps.js
+++ rps.js
@@ -1,17 +1,60 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice(){
     let NumAuxiliar = parseInt(Math.random()*10);
-    let answer = "er";
+    let answer = "";
 
-    if(NumAuxiliar === 0){
-        answer = "No me pude decidir, tu ganas";
-    } else if (NumAuxiliar > 0 && NumAuxiliar <= 3){
-        answer = "Rock";
+    if (NumAuxiliar >= 0 && NumAuxiliar <= 3){
+        answer = "piedra";
     } else if (NumAuxiliar > 3 && NumAuxiliar <= 6){
-        answer = "Paper";
-    } else if (NumAuxiliar > 7 && NumAuxiliar <= 9){
-        answer = "Scissor";
+        answer = "papel";
+    } else {
+        answer = "tijera";
     }
     return answer
 }
 
-console.log(getComputerChoice());
+
+function getHumanChoice(){
+    const HumanChoice = prompt("piedra, papel o tijera?");
+    return HumanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    let result = "Empate!";
+    const lowerhumanChoice = humanChoice.toLowerCase()
+    if (lowerhumanChoice === "piedra"){
+        if (computerChoice === "papel"){
+            computerScore++;
+            result = "Perdiste! papel le gana a piedra";
+        }   else if (computerChoice === "tijera"){
+            humanScore++;
+            result = "Ganaste! piedra le gana a tijera";
+        } 
+    } else if (lowerhumanChoice === "papel"){
+        if (computerChoice === "tijera"){
+            computerScore++;
+            result = "Perdiste! tijera le gana a papel";
+        }   else if (computerChoice === "piedra"){
+            humanScore++;
+            result = "Ganaste! papel le gana a piedra";
+        } 
+    } else if (lowerhumanChoice === "tijera"){
+        if (computerChoice === "piedra"){
+            computerScore++;
+            result = "Perdiste! piedra le gana a tijera";
+        }   else if (computerChoice === "papel"){
+            humanScore++;
+            result = "Ganaste! tijera le gana a papel";
+        } 
+    }
+
+    return result;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
